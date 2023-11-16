dc1f1c0fb31cfafef5c479559a1126eec7bd2ba9
Ashukr321 <coderashukr321@gmail.com>
2023-11-16 08:06:20 +0530

    login is completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,57 +4,123 @@ const { comparePassword, hashPassword } = require("../helpers/authHelper.js");
 
 const registerController = async (req, res) => {
   try {
-    const { name, email, password, phone, address} = req.body;
-    //validations
-    if (!name) {
-      return res.send({ error: "Name is Required" });
+    const { name, email, password, phone, address } = req.body;
+    // validate  all data name , email, password,phone ,address;
+    if (!name) { 
+      return res.send({ error: " Name is required" });
     }
     if (!email) {
-      return res.send({ error: "Email is Required" });
+      return res.send({ error: "Email is required" });
     }
     if (!password) {
-      return res.send({ error: "Password is Required" });
+      return res.send({ error: "password is required" });
     }
     if (!phone) {
-      return res.send({ error: "Phone no is Required" });
+      return res.send({ error: "phone is required" });
     }
     if (!address) {
-      return res.send({ error: "Address is Required" });
+      return res.send({ error: "address is required" });
     }
-    //check user
-    const exisitingUser = await userModel.findOne({ email });
-    //exisiting user
-    if (exisitingUser) {
+
+    //  check user exits or not
+    const existUser = await userModel.findOne({ email });
+
+    if (existUser) {
       return res.status(200).send({
         success: true,
         message: "Already Register please login",
       });
     }
-    //register user
+
     const hashedPassword = await hashPassword(password);
-    //save
-    const user = await new userModel({
+
+    const newUser = await new userModel({
       name,
       email,
       phone,
-      address,
       password: hashedPassword,
-    }).save();
+      address,
+      // }).save();
+    });
+    await newUser.save();
 
-    res.status(201).json({
+    res.status(201).send({
       success: true,
-      message: "User Register Successfully",
-      user,
+      message: "Registered successfully",
+      newUser,
     });
   } catch (error) {
     console.log(error);
-    res.status(500).json({
+    res.status(500).send({
       success: false,
-      message: "Errro in Registeration",
+      message: "Error in Registration",
       error,
     });
   }
 };
 
 
-module.exports = { registerController };
+// login  for login we use just email and password 
+
+const loginController =async (req,res)=>{
+  try{
+    // destructure the email and password from the users
+    const {email,password}=  req.body;
+    //  validate name and password 
+    if(!email||!password){
+      return res.status(404).send(
+        {
+          success:false,
+          message:"Invalid email and password"
+        }
+      )
+    }
+    //  check user exitsornot 
+    const user = await userModel.findOne({email});
+    if(!user){
+      return res.status(404).send({
+        success:false,
+        message:"Email not Registerd",
+      })
+    }
+
+    //  match the  password
+    const match = await comparePassword(password,user.password);
+    if(!match){
+      return res.status(200).send({
+        success:true,
+        message:"Invalid Password"
+      })
+    }
+
+    // token 
+    const token = await jwt.sign({_id:user.id},process.env.JWT_SECRET,{
+      expiresIn:"7d",
+    })
+
+    res.status(200).send({
+      success:true,
+      message:"login Successfully",
+      user:{
+        _id:user._id,
+         name :user.name,
+         email:user.email,
+         phone:user.phone,
+         address:user.address
+
+      }
+      ,token
+    })
+  }catch(error){
+    console.log(error);
+    res.status(500).send({
+      success:false,
+      message:"Error in Login",
+      error
+    })
+  }
+}
+module.exports = { registerController ,loginController};
+
+
+
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { registerController } = require('../controllers/authController');
+const { registerController, loginController } = require('../controllers/authController');
 const router  = express.Router();
 
 //REGISTER || METHOD POST
 router.post('/register',registerController);
+router.post('/login',loginController );
 
 module.exports = router;
